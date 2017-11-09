73bf9aca573b8b1dbf6d88cafd755fef0fe55d42
Heber Parrucci <heber.parrucci@canonical.com>
2017-11-09 10:31:44 -0300

    Initial commit for gnome-shell-tests

---
+++ run_gnome_shell_tests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import argparse
+
+import dbus
+
+
+class Client:
+
+    def __init__(self):
+        bus = dbus.SessionBus()
+        service = bus.get_object('org.gnome.Shell.TestHelper', "/org/gnome/Shell/TestHelper")
+        self._execute = service.get_dbus_method('Execute', 'org.gnome.Shell.TestHelper')
+
+    def run(self, tests, output_file):
+        self._execute(tests, output_file)
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description=
+                                     'Run gnome-shell integration tests')
+    PARSER.add_argument('--tests', help='Tests to run')
+    PARSER.add_argument('--output_file', default='',
+                        help='File path to store the results')
+    ARGS = PARSER.parse_args()
+    Client().run(ARGS.tests, ARGS.output_file)
